5f1675d7d38da1b6fad3b63ac47e3b6fa5f435b8
Scott Edwards <scotteza@gmail.com>
2022-06-20 20:45:00 +0100

    Make steer speed and move speed serializable

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+
+// ReSharper disable ArrangeTypeMemberModifiers
 // ReSharper disable CheckNamespace
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedMember.Local
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float _steeringSpeed = 1f;
+
+    [SerializeField]
+    float _movementSpeed = 0.02f;
+
     private void Start()
     {
     }
 
     private void Update()
     {
-        transform.Rotate(0, 0, 0.2f);
-        transform.Translate(0, 0.02f, 0);
+        transform.Rotate(0, 0, _steeringSpeed);
+        transform.Translate(0, _movementSpeed, 0);
     }
 }
