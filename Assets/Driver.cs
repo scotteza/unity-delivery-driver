3a510bdf93ab0890ae82e9343c994b7bc1d0f8dd
Scott Edwards <scotteza@gmail.com>
2022-06-20 20:17:07 +0100

    Rotate car

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
 
 public class Driver : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Rotate(0, 0, 0.1f);
     }
 }
