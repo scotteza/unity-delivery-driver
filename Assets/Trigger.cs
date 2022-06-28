0ff090614265bd0c1ad081baa4376e9dd10253e3
Scott Edwards <scotteza@gmail.com>
2022-06-28 12:09:45 +0100

    Driving over packages changes car color

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 // ReSharper disable CheckNamespace
+// ReSharper disable FieldCanBeMadeReadOnly.Local
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedMember.Local
 
@@ -11,6 +12,15 @@ public class Trigger : MonoBehaviour
 
     private int _carriedPackageCount = 0;
     [SerializeField] private float _destroyDelay = 0.5f;
+    [SerializeField] private Color32 _hasPackageColor = new Color32(0, 255, 0, 255);
+    [SerializeField] private Color32 _noPackageColor = new Color32(255, 255, 255, 255);
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -29,6 +39,7 @@ public class Trigger : MonoBehaviour
     private void PickUpPackage(GameObject package)
     {
         _carriedPackageCount++;
+        _spriteRenderer.color = _hasPackageColor;
         Destroy(package, _destroyDelay);
         Debug.Log("Picked up a package");
     }
@@ -43,6 +54,7 @@ public class Trigger : MonoBehaviour
         {
             Debug.Log($"Delivered {_carriedPackageCount} package(s) to the customer, good job!");
             _carriedPackageCount = 0;
+            _spriteRenderer.color = _noPackageColor;
         }
     }
 }
